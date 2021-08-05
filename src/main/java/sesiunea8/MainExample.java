008ac7a3a714c0add99b766224ed1a8f5e0fcb94
Alexandru Grigorescu <grigorescualexandru95@yahoo.ro>
2021-08-05 06:24:22 +0200

    Initial commit

--- src/main/java/sesiunea8/MainExample.java
+++ src/main/java/sesiunea8/MainExample.java
@@ -1,19 +1,8 @@
 package sesiunea8;
 
+
 public class MainExample {
 
-
-    public static void main(String[] args) {
-        printName("sorin");
-        printName("alex");
-        printName(1);
-    }
-
-    public static void printName(int i){
-        System.out.println("nu este un nume");
-    }
-
-    public static void printName(String name) {
-        System.out.println("numele lui este " + name);
+public static void main(String[] args) {
     }
 }
