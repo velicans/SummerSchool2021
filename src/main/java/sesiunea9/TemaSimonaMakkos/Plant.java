eef0f65846a3f53369f73ef5cb66cfd473c50d15
SimonaMakkos <makkos.simona.elena@gmail.com>
2021-08-08 19:29:47 +0300

    Tema Simona Makkos

--- src/main/java/sesiunea9/TemaSimonaMakkos/Plant.java
+++ src/main/java/sesiunea9/TemaSimonaMakkos/Plant.java
@@ -2,5 +2,13 @@ package sesiunea9.TemaSimonaMakkos;
 
 public abstract class Plant {
 
+    public int oxygenProduction;
 
+    public int getOxygenProduction() {
+        return oxygenProduction;
+    }
+
+    public void setOxygenProduction(int oxygenProduction) {
+        this.oxygenProduction = oxygenProduction;
+    }
 }
