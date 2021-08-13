980d28397a7e3853436a171d2fcf841a688d6eee
DanielZah <zahariadaniel96@yahoo.com>
2021-08-13 17:31:53 +0300

    tema sesiunea 12

--- src/test/java/sesiunea12/WineTest.java
+++ src/test/java/sesiunea12/WineTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 public class WineTest extends BaseTest {
 
     WineyardPO winePage = new WineyardPO(driver);
+    public static final String GRAPE_NAME = "DanielGrapeV6";
 
     @BeforeEach
     public void openPage() {
@@ -31,4 +32,71 @@ public class WineTest extends BaseTest {
         int finalValue = winePage.countWines();
         Assertions.assertTrue(finalValue - initialValue == 1, "New grape was NOT added successfully!");
     }
+
+
+    //Check that total rows value and type of grapes value are matching the values in table.
+    @Test
+    public void checkRowsTypes(){
+
+        Assertions.assertTrue(winePage.checkRowsTypes(),"Total rows value and/or type of grapes value do NOT match the values from the table!");
+
+    }
+
+
+    //Add a new type of grape with ripeness of 97%.
+    //Check that total rows value and type of grapes value are updated.
+    @Test
+    public void addNewGrapeV2(){
+
+        int initialValue = winePage.countWines();
+        winePage.addNewGrapeV2(GRAPE_NAME,"36","rows","80","97");
+        int finalValue = winePage.countWines();
+        Assertions.assertTrue(finalValue - initialValue == 1, "New grape was NOT added successfully!");
+        Assertions.assertTrue(winePage.checkRowsTypes(),"Total rows value and/or type of grapes value have NOT been updated!");
+
+    }
+
+
+    //Pick up and crush your newly added grape.
+    //Check that must count value and must total volume matches the values from the table.
+    @Test
+    public void pickAndCrush(){
+
+        winePage.pickAndCrush(GRAPE_NAME);
+        Assertions.assertTrue(winePage.checkMustCountVolume(),"Must count value and/or must total volume do NOT match the values from the table!");
+
+    }
+
+
+    //Select your must and Ferment it.
+    //Check that wines value and volume value matches the data displayed in table
+    @Test
+    public void selectAndFerment(){
+
+        winePage.selectAndFerment(GRAPE_NAME);
+        Assertions.assertTrue(winePage.checkWinesCountVolume(),"Wines count value and/or wines total volume do NOT match the values from the table!");
+
+    }
+
+
+    //Go back to Must page and check that your must is missing and that the values have been updated.
+    @Test
+    public void checkMustPage(){
+
+        Assertions.assertTrue(winePage.checkMustMissing(GRAPE_NAME),"Must has NOT been removed from the table!");
+        Assertions.assertTrue(winePage.checkMustCountVolume(),"Must count value and/or must total volume have NOT been updated!");
+
+    }
+
+
+    //Go back to Grapes page and check that your grape is missing and that the values have been updated.
+    @Test
+    public void checkGrapePage(){
+
+        Assertions.assertTrue(winePage.checkGrapeMissing(GRAPE_NAME),"Grape has NOT been removed from the table!");
+        Assertions.assertTrue(winePage.checkRowsTypes(),"Grapes count and/or rows values have NOT been updated");
+
+    }
+
+
 }
--- src/test/java/sesiunea12/WineyardPO.java
+++ src/test/java/sesiunea12/WineyardPO.java
@@ -26,7 +26,19 @@ public class WineyardPO {
     public String mustRows = "table > tbody >tr";
     public String fermentBtn = "button";
     public String wineRows = "table > tbody >tr";
-    public String wineRows2 = "table > tbody >tr";
+    public String grapesPage = "a:nth-child(2)";
+    public String typeOfGrapes = "li:nth-child(1)";
+    public String totalRows = "li:nth-child(2)";
+    public String grapeQuantity = "td:nth-child(2)";
+    public String mustCount = "li:nth-child(1)";
+    public String mustTotalVolume ="li:nth-child(2)";
+    public String mustVolume = "td:nth-child(3)";
+    public String mustPage = "a:nth-child(3)";
+    public String winePage = "a:nth-child(4)";
+    public String winesCount = "li:nth-child(1)";
+    public String winesTotalVolume = "li:nth-child(2)";
+    public String winesVolume = "td:nth-child(3)";
+
 
     public WineyardPO(WebDriver driver) {
         this.driver = driver;
@@ -72,4 +84,238 @@ public class WineyardPO {
 
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(submitBtn))).click();
     }
+
+    //move to Grapes page
+    public void moveToGrapesPage(){
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(grapesPage))).click();
+    }
+
+    //Check that total rows value and type of grapes value are matching the values in table.
+    public boolean checkRowsTypes(){
+
+        boolean rowsTypesMatch = true;
+        int numberOfGrapes = 0;
+        int numberOfRows = 0;
+        int numberOfRowsTable = 0;
+
+        numberOfGrapes = Integer.parseInt(wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(typeOfGrapes))).getText().replace("Types of grapes: ",""));
+
+        if(numberOfGrapes != countWines()){
+            rowsTypesMatch = false;
+        }
+
+        if (rowsTypesMatch == true) {
+
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(wineRows)));
+            List<WebElement> list = driver.findElements(By.cssSelector(wineRows));
+
+            numberOfRows = Integer.parseInt(wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(totalRows))).getText().replace("Total rows: ",""));
+
+            for(WebElement row : list) {
+
+                numberOfRowsTable = numberOfRowsTable + Integer.parseInt(row.findElement(By.cssSelector(grapeQuantity)).getText().replace(" rows","").replace(" barrows","").replace(" cases",""));
+
+            }
+
+            if(numberOfRows != numberOfRowsTable){
+                rowsTypesMatch = false;
+            }
+
+        }
+        return rowsTypesMatch;
+    }
+
+    //Add a new type of grape with ripeness of 97%.
+    public void addNewGrapeV2(String grapeNameHelp, String grapeQuantityHelp, String grapeUnitHelp, String grapeAgeHelp, String grapeRipenessHelp){
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(addGrapesBtn))).click();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(name))).clear();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(name))).sendKeys(grapeNameHelp);
+
+        // define select for quantity dropdown
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(quantity)));
+        Select quantitySelect = new Select(driver.findElement(By.cssSelector(quantity)));
+        quantitySelect.selectByVisibleText(grapeQuantityHelp);
+
+
+        // define select for unit dropdown
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(unit)));
+        Select unitSelect = new Select(driver.findElement(By.cssSelector(unit)));
+        unitSelect.selectByVisibleText(grapeUnitHelp);
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(age))).clear();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(age))).sendKeys(grapeAgeHelp);
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(ripeness))).clear();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(ripeness))).sendKeys(grapeRipenessHelp);
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(submitBtn))).click();
+    }
+
+    //Pick up and crush your newly added grape.
+    public void pickAndCrush(String myGrape){
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(wineRows)));
+        List<WebElement> list = driver.findElements(By.cssSelector(wineRows));
+
+        for(WebElement row : list) {
+
+            if(row.findElements(By.tagName("td")).get(0).getText().equals(myGrape)){
+                row.findElement(By.tagName("button")).click();
+                break;
+            }
+
+
+        }
+
+    }
+
+    //move to Must page
+    public void moveToMustPage(){
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(mustPage))).click();
+    }
+
+    //Check that must count value and must total volume matches the values from the table.
+    public boolean checkMustCountVolume(){
+
+        moveToMustPage();
+
+        boolean mustCountVolumeMatch = true;
+        int mustNumber = 0;
+        int volumeNumber = 0;
+        int volumeNumberTable = 0;
+
+        mustNumber = Integer.parseInt(wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(mustCount))).getText().replace("Must count: ",""));
+
+        if (mustNumber != countWines()){
+            mustCountVolumeMatch = false;
+        }
+
+        if (mustCountVolumeMatch == true){
+
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(wineRows)));
+            List<WebElement> list = driver.findElements(By.cssSelector(wineRows));
+
+            volumeNumber = Integer.parseInt(wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(mustTotalVolume))).getText().replace("Must total volume: ","").replace(" liters",""));
+
+            for(WebElement row : list) {
+
+                volumeNumberTable = volumeNumberTable + Integer.parseInt(row.findElement(By.cssSelector(mustVolume)).getText().replace(" liters",""));
+
+            }
+
+            if(volumeNumber != volumeNumberTable){
+                mustCountVolumeMatch = false;
+            }
+
+        }
+
+        return mustCountVolumeMatch;
+    }
+
+    //Select your must and Ferment it.
+    public void selectAndFerment(String myGrape){
+
+        moveToMustPage();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(wineRows)));
+        List<WebElement> list = driver.findElements(By.cssSelector(wineRows));
+
+        for(WebElement row : list) {
+
+            if (row.findElements(By.tagName("td")).get(1).getText().equals(myGrape)) {
+                row.findElement(By.cssSelector("input[type=checkbox]")).click();
+                break;
+            }
+        }
+
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(fermentBtn))).click();
+
+    }
+
+    //move to Wines page
+    public void moveToWinePage(){
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(winePage))).click();
+    }
+
+    //Check that wines value and volume value matches the data displayed in table.
+    public boolean checkWinesCountVolume(){
+
+        moveToWinePage();
+
+        boolean winesCountVolumeMatch = true;
+        int numberOfWines = 0;
+        int volumeNumber = 0;
+        int volumeNumberTable = 0;
+
+        numberOfWines = Integer.parseInt(wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(winesCount))).getText().replace("Wines: ","").replace(" wines",""));
+
+        if (numberOfWines != countWines()){
+            winesCountVolumeMatch = false;
+        }
+
+        if (winesCountVolumeMatch == true){
+
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(wineRows)));
+            List<WebElement> list = driver.findElements(By.cssSelector(wineRows));
+
+            volumeNumber = Integer.parseInt(wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(winesTotalVolume))).getText().replace("Volume: ","").replace(" liters",""));
+
+            for(WebElement row : list) {
+
+                volumeNumberTable = volumeNumberTable + Integer.parseInt(row.findElement(By.cssSelector(winesVolume)).getText().replace(" liters",""));
+
+            }
+
+            if(volumeNumber != volumeNumberTable){
+                winesCountVolumeMatch = false;
+            }
+
+        }
+
+        return winesCountVolumeMatch;
+    }
+
+    //Check that your must is missing
+    public boolean checkMustMissing(String myGrape){
+
+        moveToMustPage();
+
+        boolean mustIsMissing = true;
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(wineRows)));
+        List<WebElement> list = driver.findElements(By.cssSelector(wineRows));
+
+        for(WebElement row : list) {
+
+            if(row.findElements(By.tagName("td")).get(1).getText().equals(myGrape)){
+                mustIsMissing = false;
+                break;
+            }
+
+        }
+
+        return mustIsMissing;
+    }
+
+    //Check that your grape is missing
+    public boolean checkGrapeMissing(String myGrape){
+
+        boolean grapeIsMissing = true;
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(wineRows)));
+        List<WebElement> list = driver.findElements(By.cssSelector(wineRows));
+
+        for(WebElement row : list) {
+
+            if(row.findElements(By.tagName("td")).get(0).getText().equals(myGrape)){
+                grapeIsMissing = false;
+                break;
+            }
+
+        }
+
+        return grapeIsMissing;
+    }
+
 }
