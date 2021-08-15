76d1d8544cdd7899618407dff41d9d8e2e36641f
MonicaPaltanea <monica.paltanea@gmail.com>
2021-08-15 21:27:24 +0300

    Session12 - Homework

--- src/test/java/sesiunea12/WineyardPO.java
+++ src/test/java/sesiunea12/WineyardPO.java
@@ -27,6 +27,13 @@ public class WineyardPO {
     public String fermentBtn = "button";
     public String wineRows = "table > tbody >tr";
     public String wineRows2 = "table > tbody >tr";
+    public String typeOfGrapesNr = "//body/div[@id='root']/div[1]/section[1]/ul[1]/li[1]";
+    public String totalRows = "//body/div[@id='root']/div[1]/section[1]/ul[1]/li[2]";
+    public String mustCount = "//body/div[@id='root']/div[1]/section[1]/ul[1]/li[1]";
+    public String mustTotalVolume="//body[1]/div[1]/div[1]/section[1]/ul[1]/li[2]";
+    public String winesValue="//body/div[@id='root']/div[1]/section[1]/ul[1]/li[1]";
+    public String winesVolume="//body[1]/div[1]/div[1]/section[1]/ul[1]/li[2]";
+    public static final String GRAPE_NAME = "Grapy10";
 
     public WineyardPO(WebDriver driver) {
         this.driver = driver;
@@ -41,17 +48,17 @@ public class WineyardPO {
         return isHomePageOpened;
     }
 
-    public int countWines(){
+    public int countWines() {
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(wineRows)));
         List<WebElement> list = driver.findElements(By.cssSelector(wineRows));
         return list.size();
     }
 
-    public void addNewGrape(){
+    public void addNewGrape() {
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(addGrapesBtn))).click();
 
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(name))).clear();
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(name))).sendKeys("testName");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(name))).sendKeys(GRAPE_NAME);;
 
         // define select for quantity dropdown
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(quantity)));
@@ -68,8 +75,162 @@ public class WineyardPO {
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(age))).sendKeys("3");
 
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(ripeness))).clear();
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(ripeness))).sendKeys("98");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(ripeness))).sendKeys("97");
 
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(submitBtn))).click();
     }
-}
+
+    public void pickUpAndCrush() {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(tableRows)));
+        List<WebElement> rows = driver.findElements(By.cssSelector(tableRows));
+        for (WebElement row : rows) {
+            if (row.findElements(By.tagName("td")).get(0).getText().equals(GRAPE_NAME)) {
+                row.findElement(By.tagName("button")).click();
+                wait.until(ExpectedConditions.urlContains("/must"));
+                break;
+            }
+        }
+
+
+    }
+
+    public void selectAndFerment(){
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(tableRows)));
+        List<WebElement> rows = driver.findElements(By.cssSelector(tableRows));
+        System.out.println("Number of rows: " + rows.size());
+        for(WebElement row : rows) {
+            System.out.println("Grape name: " + row.findElements(By.tagName("td")).get(1).getText());
+            if(row.findElements(By.tagName("td")).get(1).getText().equals(GRAPE_NAME)){
+                WebElement checkbox = row.findElement(By.tagName("input"));
+                System.out.println("Is selected:" + checkbox.isSelected());
+                if(!checkbox.isSelected())
+                {
+                    checkbox.click();
+                    System.out.println("Checkbox clicked");
+                }
+                break;
+            }
+        }
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(fermentBtn))).click();
+        System.out.println("Ferment clicked");
+        wait.until(ExpectedConditions.urlContains("/wines"));
+    }
+
+    public int getSumRows()
+    {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(tableRows)));
+        List<WebElement> rows = driver.findElements(By.cssSelector(tableRows));
+        int sum = 0;
+        for (WebElement row : rows) {
+            WebElement rowQuantity= row.findElements(By.tagName("td")).get(1);
+            sum = sum + Integer.parseInt(rowQuantity.getText().replaceAll("[\\D]", ""));
+        }
+        return sum;
+    }
+
+    public int getTotalRows()
+    {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(totalRows)));
+        String typeOfGrapesValue= driver.findElement(By.xpath(totalRows)).getText();
+        return Integer.parseInt(typeOfGrapesValue.replaceAll("[\\D]", ""));
+    }
+
+    public int getNumberOfTypes()
+    {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(typeOfGrapesNr)));
+        String typeOfGrapesValue=driver.findElement(By.xpath(typeOfGrapesNr)).getText();
+        return Integer.parseInt(typeOfGrapesValue.replaceAll("[\\D]", ""));
+    }
+
+    public int getMustCount(){
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(tableRows)));
+        String mustCountValue=driver.findElement(By.xpath(mustCount)).getText();
+        return Integer.parseInt(mustCountValue.replaceAll("[\\D]", ""));
+
+    }
+    public int getMustVolume(){
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(tableRows)));
+        String mustTotalVolumeValue=driver.findElement(By.xpath(mustTotalVolume)).getText();
+        return Integer.parseInt(mustTotalVolumeValue.replaceAll("[\\D]", ""));
+
+    }
+
+    public int getSumMustRows()
+    {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(tableRows)));
+        List<WebElement> rows = driver.findElements(By.cssSelector(tableRows));
+        int sum = 0;
+        for (WebElement row : rows) {
+            WebElement rowQuantity= row.findElements(By.tagName("td")).get(2);
+            sum = sum + Integer.parseInt(rowQuantity.getText().replaceAll("[\\D]", ""));
+        }
+        return sum;
+    }
+    public int getWinesNr(){
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(tableRows)));
+        String winesNumber=driver.findElement(By.xpath(winesValue)).getText();
+        return Integer.parseInt(winesNumber.replaceAll("[\\D]", ""));
+
+    }
+
+    public int getWinesVolumeNr(){
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(tableRows)));
+        String winesVolumeNumber=driver.findElement(By.xpath(winesVolume)).getText();
+        return Integer.parseInt(winesVolumeNumber.replaceAll("[\\D]", ""));
+
+    }
+
+    public boolean mustIsmissing(){
+        boolean isMustMissing = true;
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(tableRows)));
+        List<WebElement> rows = driver.findElements(By.cssSelector(tableRows));
+        for (WebElement row : rows) {
+            if (row.findElements(By.tagName("td")).get(1).getText().equals(GRAPE_NAME)) {
+                isMustMissing=false;
+                break;
+            }
+        }
+    return isMustMissing;
+
+    }
+
+    public boolean grapeIsMissing(){
+        boolean isGrapeMissing = true;
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(tableRows)));
+        List<WebElement> rows = driver.findElements(By.cssSelector(tableRows));
+        for (WebElement row : rows) {
+            if (row.findElements(By.tagName("td")).get(0).getText().equals(GRAPE_NAME)) {
+                isGrapeMissing=false;
+                break;
+            }
+        }
+        return isGrapeMissing;
+
+    }
+
+    public void goToMust()
+    {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[contains(text(),'Must')]"))).click();
+        wait.until(ExpectedConditions.urlContains("/must"));
+    }
+
+    public void goToWines()
+    {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[contains(text(),'Wines')]"))).click();
+        wait.until(ExpectedConditions.urlContains("/wines"));
+    }
+
+    public void goToGrapes()
+    {
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[contains(text(),'Grapes')]"))).click();
+        wait.until(ExpectedConditions.urlContains("/grapes"));
+    }
+
+
+
+ }
